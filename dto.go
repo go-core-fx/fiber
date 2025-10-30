272069ffbe9466888fdca6d241ef0676ddcfb9cb
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-10-30 15:36:00 +0700

    [dto] simplify error response

--- dto.go
+++ dto.go
@@ -1,21 +1,15 @@
 package fiberfx
 
-type Error struct {
+type ErrorResponse struct {
 	Message string `json:"message"`
 	Code    int    `json:"code,omitempty"`
 	Details any    `json:"details,omitempty"`
 }
 
-type ErrorResponse struct {
-	Error Error `json:"error"`
-}
-
 func NewErrorResponse(message string, code int, details any) ErrorResponse {
 	return ErrorResponse{
-		Error: Error{
-			Message: message,
-			Code:    code,
-			Details: details,
-		},
+		Message: message,
+		Code:    code,
+		Details: details,
 	}
 }
